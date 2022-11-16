d2fdf2aa756c18a885bf29aa381c265c8fcbcd18
User <User@DESKTOP-RDG3K1V>
2022-11-16 14:03:42 +0100

    updated

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -9,7 +9,7 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) throws Exception {
+	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
 		try {
 			File dir = new File (mappe);
